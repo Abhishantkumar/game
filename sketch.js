5740375d3ff168d60d4afed8c221670c277b9e26
Abhishant Kumar <mikkesingh6202@gmail.com>
2021-01-17 16:53:12 +0530

    new

--- sketch.js
+++ sketch.js
@@ -9,7 +9,15 @@ var database;
 var form, player, game;
 
 var cars, car1, car2, car3, car4;
-
+function preload(){
+  car1_IMG=loadImage("images/car1.png");
+  car2_IMG=loadImage("images/car2.png");
+  car3_IMG=loadImage("images/car3.png");
+  car4_IMG=loadImage("images/car4.png");
+  ground1=loadImage("images/ground.png");
+  track=loadImage("images/track.jpg");
+  
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
@@ -27,5 +35,9 @@ function draw(){
   if(gameState === 1){
     clear();
     game.play();
+  }
+  if(gameState===2){
+    game.end()
+    
   }
 }
